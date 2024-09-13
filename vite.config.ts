08ecd2498d0d8d72c2352d7067e3703855fcb858
Khrystyna <khrystyna.makar@gmail.com>
2024-09-13 09:38:04 +0300

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    include: /\.(jsx|tsx)$/,
-    babel: {
-      plugins: ['styled-components'],
-      babelrc: false,
-      configFile: false,
-    },
-  })],
+  plugins: [react()],
+  base: '/Todo-app'
 })
